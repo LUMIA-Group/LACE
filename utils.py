ec45b3d035c3ef7857af8279e2f1206d33089c39
Expocen <huangyusheng@sjtu.edu.cn>
2023-10-06 16:56:13 +0800

    send.

--- utils.py
+++ utils.py
@@ -2,14 +2,12 @@ import torch
 import random
 import numpy as np
 
-
 def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     if args.n_gpu > 0 and torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
-
 
 def collate_fn(batch):
     max_len = max([len(f["input_ids"]) for f in batch])
